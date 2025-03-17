6419566fa4af3e13d392b8e23f7fa47becd560e0
Peter <you@example.com>
2025-03-17 17:49:27 +0100

    Petya

--- VideoGame/Game.cs
+++ VideoGame/Game.cs
@@ -8,7 +8,17 @@ namespace VideoGame
 {
     public class Game
     {
-     
+        public string GameID { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int RelaseYear { get; set; }
+        public bool IsAvailable { get; set;
+        public decimal PricePerDay { get; set; }
+
+        public override string? ToString()
+        {
+            return base.ToString();
+        }
     }
 
 }
